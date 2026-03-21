0f007b005a3cebe2e9456960d746a7fab0f7dfc3
gambacanastando <brpuig05@gmail.com>
2026-03-21 11:14:31 +0100

    error

--- Backend/lambda_function.py
+++ Backend/lambda_function.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HF_TOKEN = "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"
+HF_TOKEN = "token"
 LLM_API_URL = "https://api-inference.huggingface.co/models/modelo-llm"
 PREDICT_API_URL = "https://api-inference.huggingface.co/models/modelo-predictivo"
 
